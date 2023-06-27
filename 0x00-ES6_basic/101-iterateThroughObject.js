a0b0986f815a14c36a279dcf93827ae506375841
Sireat <102405529+Sireat@users.noreply.github.com>
2023-06-27 09:17:23 +0300

    correct 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,12 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-    let result = '';
-    let iteration = reportWithIterator.next();
-    
-    while (!iteration.done) {
-      result += `${iteration.value} | `;
-      iteration = reportWithIterator.next();
-    }
-    
-    return result.slice(0, -3); // Remove the trailing " | "
-  }
-  
+  return reportWithIterator.join(' | ');
+}
