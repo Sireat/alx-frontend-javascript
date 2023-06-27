d07aabb617c94cb7548767f83c72259d58fcdc37
Sireat <102405529+Sireat@users.noreply.github.com>
2023-06-27 09:14:17 +0300

    correct 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,28 +1,12 @@
 export default function createIteratorObject(report) {
-  const allEmployees = report.allEmployees;
-  const departments = Object.keys(allEmployees);
-  let currentDepartmentIndex = 0;
-  let currentEmployeeIndex = 0;
+  const emps = [];
+  // eslint-disable-next-line
+  for (const dep of Object.keys(report.allEmployees)) {
+    // eslint-disable-next-line
+    for (const emp of report.allEmployees[dep]) {
+      emps.push(emp);
+    }
+  }
 
-  return {
-    next: function () {
-      if (currentDepartmentIndex >= departments.length) {
-        return { done: true };
-      }
-
-      const departmentName = departments[currentDepartmentIndex];
-      const employees = allEmployees[departmentName];
-      
-      if (currentEmployeeIndex >= employees.length) {
-        currentDepartmentIndex++;
-        currentEmployeeIndex = 0;
-        return this.next();
-      }
-
-      const employee = employees[currentEmployeeIndex];
-      currentEmployeeIndex++;
-
-      return { value: employee, done: false };
-    },
-  };
+  return emps;
 }
